423875db6bdf199c4d8b2b217d39180d85626d5b
DiepPk <kdt310722@gmail.com>
2025-08-12 11:50:48 +0700

    refactor: rename JSON-RPC factory methods

--- src/json-rpc/factories.ts
+++ src/json-rpc/factories.ts
@@ -1,18 +1,18 @@
 import type { JsonRpcErrorObject, JsonRpcErrorResponseMessage, JsonRpcNotifyMessage, JsonRpcRequestMessage, JsonRpcResponseId, JsonRpcSuccessResponseMessage } from './types'
 import { notUndefined } from '../core'
 
-export function createNotifyMessage<TParams>(method: string, params?: TParams): JsonRpcNotifyMessage<TParams> {
+export function createJsonRpcNotifyMessage<TParams>(method: string, params?: TParams): JsonRpcNotifyMessage<TParams> {
     return { jsonrpc: '2.0', method, ...(notUndefined(params) ? { params } : {}) }
 }
 
-export function createRequestMessage<TParams>(id: string | number, method: string, params?: TParams): JsonRpcRequestMessage<TParams> {
+export function createJsonRpcRequestMessage<TParams>(id: string | number, method: string, params?: TParams): JsonRpcRequestMessage<TParams> {
     return { jsonrpc: '2.0', id, method, ...(notUndefined(params) ? { params } : {}) }
 }
 
-export function createSuccessResponseMessage<R = any>(id: JsonRpcResponseId, result: R): JsonRpcSuccessResponseMessage<R> {
+export function createJsonRpcSuccessResponseMessage<R = any>(id: JsonRpcResponseId, result: R): JsonRpcSuccessResponseMessage<R> {
     return { jsonrpc: '2.0', id, result }
 }
 
-export function createErrorResponseMessage<TData>(id: JsonRpcResponseId, error: JsonRpcErrorObject<TData>): JsonRpcErrorResponseMessage<TData> {
+export function createJsonRpcErrorResponseMessage<TData>(id: JsonRpcResponseId, error: JsonRpcErrorObject<TData>): JsonRpcErrorResponseMessage<TData> {
     return { jsonrpc: '2.0', id, error }
 }
